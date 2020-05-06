8d2332442abb9fee550b9f3b0dceedc7ec2bc503
owl4soul <resuuser989@gmail.com>
2020-05-06 15:54:11 +0300

    Initial commit

---
+++ src/main/java/ru/spi2/sibur_webservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.spi2.sibur_webservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiburWebserviceApplication.class);
+	}
+
+}
---
+++ src/main/java/ru/spi2/sibur_webservice/SiburWebserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.spi2.sibur_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiburWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiburWebserviceApplication.class, args);
+	}
+
+}
