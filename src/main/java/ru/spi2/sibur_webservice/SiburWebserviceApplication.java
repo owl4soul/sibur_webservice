54d15d915be14a4929e3c95911056354be46e9f3
owl4soul <resuuser989@gmail.com>
2020-05-07 14:10:26 +0300

    Нужно создавать исполняемый джарник со внешними настройками

--- src/main/java/ru/spi2/sibur_webservice/SiburWebserviceApplication.java
+++ src/main/java/ru/spi2/sibur_webservice/SiburWebserviceApplication.java
@@ -1,9 +1,11 @@
 package ru.spi2.sibur_webservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class SiburWebserviceApplication {
 
 	public static void main(String[] args) {
